df4cc1529f3f92dcde5c9da1da7fc8eca459b3f8
nhhasib <nafiul.hasan1997@gmail.com>
2023-06-11 12:33:27 +0600

    classes page created

---
+++ src/Component/pages/Classes/ClassCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ClassCard = ({ data }) => {
+    console.log(data)
+    return (
+        <div>
+            <div className="card w-96 h-[650px] bg-base-100 shadow-xl">
+                <figure><img className='w-full h-[250px]' src={image} alt="Shoes" /></figure>
+                <div className="card-body text-center">
+                    <h2 className="font-bold text-3xl">{name}</h2>
+                    <h2>Email: {email}</h2>
+                    <div className='my-2'>
+                    <h2 className='font-bold underline'>{number} classes taken by {name}</h2>
+                    {classes.map(c => <p>✓ {c}</p>)}
+                    </div>
+    <div className="card-actions bottom-4 left-32 absolute">
+      <button className="button">See Classes</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default ClassCard;
--- src/Component/pages/Classes/Classes.jsx
+++ src/Component/pages/Classes/Classes.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import img from '../../../assets/header/education-concept.jpg'
+import SectionHeader from '../../shared/SectionHeader';
+import { useLoaderData } from 'react-router-dom';
+import ClassCard from './ClassCard';
 
 const Classes = () => {
+    const  classes  = useLoaderData()
     return (
         <div>
-            <h1>from classes</h1>
+            <SectionHeader img={img} title={'Classes'}></SectionHeader>
+            <div>
+                {
+                    classes.map(c=><ClassCard key={c.id} data={c}></ClassCard>)
+                }
+            </div>
         </div>
     );
 };
